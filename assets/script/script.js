2c0b3feeb7133b9ca7dc25825bb74d63305825f1
Antonio Marino <marino.antonio375@gmail.com>
2021-10-20 14:06:57 -0400

    formatted documents

--- assets/script/script.js
+++ assets/script/script.js
@@ -6,9 +6,9 @@ function getCurrentWeather(event) {
     event.preventDefault();
     console.log(city.val());
     var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city.val() + "&appid=c8426401a16cc79bc8d7d5541a0f8741&units=imperial";
-    fetch(url).then(response =>{
+    fetch(url).then(response => {
         return response.json()
-    }).then(data =>{
+    }).then(data => {
         console.log(data)
         console.log(data.main.temp);
         var temp = $("#temp");
@@ -20,17 +20,17 @@ function getCurrentWeather(event) {
         wind.text("Wind: " + data.wind.speed);
         humidity.text("Humidity: " + data.main.humidity);
         //uv.text("UV Index: " + data.)
-        cityName.html(data.name + "<img src=" + icon + " />");    
+        cityName.html(data.name + "<img src=" + icon + " />");
     })
 }
 
-function getForecast(event){
+function getForecast(event) {
     event.preventDefault();
     var urlForecast = "http://api.openweathermap.org/data/2.5/forecast?q=" + city.val() + "&appid=c8426401a16cc79bc8d7d5541a0f8741&units=imperial";
-    fetch(urlForecast).then(response =>{
+    fetch(urlForecast).then(response => {
         console.log(response);
         return response.json();
-    }).then(data =>{
+    }).then(data => {
         console.log(data);
     })
 }
