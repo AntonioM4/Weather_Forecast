5566aa3659808b12c18a2bc25499037776ee4bc3
Antonio Marino <marino.antonio375@gmail.com>
2021-10-21 12:33:12 -0400

    Getting a response from the server now

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,39 +1,23 @@
-var btn = $(".btn");
-var city = $(".form-control");
-var cityName = $("#city-name");
-
-function getCurrentWeather(event) {
-    event.preventDefault();
-    console.log(city.val());
-    var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city.val() + "&appid=c8426401a16cc79bc8d7d5541a0f8741&units=imperial";
-    fetch(url).then(response => {
-        return response.json()
-    }).then(data => {
-        console.log(data)
-        console.log(data.main.temp);
-        var temp = $("#temp");
-        var wind = $("#wind");
-        var humidity = $("#humidity")
-        var uv = $("#uv")
-        var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
-        temp.text("Temperature: " + data.main.temp);
-        wind.text("Wind: " + data.wind.speed);
-        humidity.text("Humidity: " + data.main.humidity);
-        //uv.text("UV Index: " + data.)
-        cityName.html(data.name + "<img src=" + icon + " />");
-    })
-}
-
-function getForecast(event) {
-    event.preventDefault();
-    var urlForecast = "http://api.openweathermap.org/data/2.5/forecast?q=" + city.val() + "&appid=c8426401a16cc79bc8d7d5541a0f8741&units=imperial";
-    fetch(urlForecast).then(response => {
-        console.log(response);
+var city = 'St. Louis'
+var WeatherAPIkey = 'c8426401a16cc79bc8d7d5541a0f8741';
+function getCity(){
+    city = document.querySelector('.getCity').value;
+    updateCity(city);
+ }
+ function updateCity(city){
+fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + WeatherAPIkey, {
+    // The browser fetches the resource from the remote server without first looking in the cache.
+    // The browser will then update the cache with the downloaded resource.
+    cache: 'reload',
+})
+    .then(function (response) {
         return response.json();
-    }).then(data => {
-        console.log(data);
     })
+    .then(function (data) {
+        var searchCriteria = '';
+        var weather = data;
+        var status = weather.weather[0].main;
+        console.log(data);
+    
+ })
 }
-
-btn.on("click", getCurrentWeather);
-btn.on("click", getForecast);
