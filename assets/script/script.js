4aa37f81a00d379d086f7ef11c8a7d659e3fc141
Antonio Marino <marino.antonio375@gmail.com>
2021-10-21 23:50:46 -0400

    getting information to appear in correct positions

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,8 @@
 var city = ''
+var tempEl = $('#temp')
+var windEl = $('#wind')
+var humidityEl = $('#humidity')
+var uvEl =$('#uv')
 var WeatherAPIkey = 'c8426401a16cc79bc8d7d5541a0f8741';
 function getCity() {
     city = document.querySelector('.getCity').value;
@@ -18,9 +22,26 @@ function updateCity(city) {
             var lat = data.coord.lat;
             var lon = data.coord.lon;
             var temp = data.main.temp;
-            console.log(data);
-            console.log(lat);
-            console.log(lon);
-            console.log(temp);
-        })
+            var wind = data.wind.speed;
+            var humidity = data.main.humidity
+            tempEl.append(temp + ' °F')
+            windEl.append(wind + ' mph')
+            humidityEl.append(humidity + '%')
+            latLon()
+            console.log(data)
+
+
+            function latLon() {
+                fetch('https:api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&appid=' + WeatherAPIkey, {
+                })
+                    .then(function (response) {
+                        return response.json();
+                    })
+                    .then(function (data) {
+                        var uv = data.current.uvi
+                        uvEl.append(uv)
+                        console.log(data)
+                    })
+        }
+    })
 }
