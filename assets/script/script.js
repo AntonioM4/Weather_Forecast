a81c16da614d65827b3c2bcfb1bfc636768e4af7
Antonio Marino <marino.antonio375@gmail.com>
2021-10-21 22:37:02 -0400

    getting responses and able to console log them

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,23 +1,26 @@
-var city = 'St. Louis'
+var city = ''
 var WeatherAPIkey = 'c8426401a16cc79bc8d7d5541a0f8741';
-function getCity(){
+function getCity() {
     city = document.querySelector('.getCity').value;
     updateCity(city);
- }
- function updateCity(city){
-fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + WeatherAPIkey, {
-    // The browser fetches the resource from the remote server without first looking in the cache.
-    // The browser will then update the cache with the downloaded resource.
-    cache: 'reload',
-})
-    .then(function (response) {
-        return response.json();
+}
+function updateCity(city) {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + WeatherAPIkey + '&units=imperial', {
+        cache: 'reload',
     })
-    .then(function (data) {
-        var searchCriteria = '';
-        var weather = data;
-        var status = weather.weather[0].main;
-        console.log(data);
-    
- })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            var searchCriteria = '';
+            var weather = data;
+            var tempature = data.textContent
+            var lat = data.coord.lat;
+            var lon = data.coord.lon;
+            var temp = data.main.temp;
+            console.log(data);
+            console.log(lat);
+            console.log(lon);
+            console.log(temp);
+        })
 }
